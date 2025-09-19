1beb4a16b78373fc150b493b909a0eda7a5b6938
user <user@Haliya>
2025-09-19 23:30:44 +0700

    Tugas Praktikum Alpro Pekan 2, Membuat program dengan 4 tipe data

---
+++ prakalpro_2025_B_2511532002/src/pekan2/tugasAlproPekan2.java
@@ -0,0 +1,21 @@
+package pekan2;
+
+public class tugasAlproPekan2 {
+
+	public static void main(String[] args) {
+		// Deklarasi Variabel
+		String User = "Haliya";
+		int JumlahLagu =  25;
+		float DurasiTotal = 1.8f;		
+		char Mood = 'S';				
+		boolean SedangShuffle = false;
+		
+		// Tampilkan Output
+		System.out.println("=== Data Playlist Musik ===");
+		System.out.println("User           : " + User);
+		System.out.println("Jumlah Lagu    : " + JumlahLagu);
+		System.out.println("Durasi Total   : " + DurasiTotal + "jam");
+		System.out.println("Mood           : " + Mood);
+		System.out.println("Sedang Shuffle : " + SedangShuffle);
+	}
+}
