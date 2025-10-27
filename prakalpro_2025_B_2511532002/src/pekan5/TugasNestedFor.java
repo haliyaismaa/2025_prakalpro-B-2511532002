024888d475eb6d96765c4caf6b485654d426edcd
Haliya Isma Husna Putri Ahmadi <haliyaismaa@gmail.com>
2025-10-27 11:59:46 +0700

    Add files via upload
    Tugas Pekan 5 Perulangan For & Nested For

---
+++ prakalpro_2025_B_2511532002/src/pekan5/TugasNestedFor.java
@@ -0,0 +1,16 @@
+package pekan5;
+
+public class TugasNestedFor {
+
+	public static void main(String[] args) {
+		for (int line = 1; line <= 5; line++) {
+			for (int j = 1; j <= (-1 * line + 5); j++) {
+				if (int i+j =j)
+			}
+			System.out.print(line);
+			System.out.println();
+		}
+	}
+
+}
+ 
