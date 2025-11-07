94912b8cf0bf3f966774a325ab0c716ea69db384
Haliya Isma Husna Putri Ahmadi <haliyaismaa@gmail.com>
2025-11-07 21:43:57 +0700

    Add files via upload
    Tugas Praktikum Pekan-6

---
+++ prakalpro_2025_B_2511532002/src/pekan6_2511532002/tugasAlproPekan6_2511532002.java
@@ -0,0 +1,45 @@
+package pekan6_2511532002;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class tugasAlproPekan6_2511532002 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner (System.in);
+		Random rand = new Random();
+		
+		String answer;
+		int dadu1, dadu2, hasil;
+		int tries=0;
+		int sum=0;
+		boolean running=true;
+		
+		System.out.println("===== Permainan Lempar Dadu =====");
+		do {
+		dadu1 = rand.nextInt (6)+1;
+		dadu2 = rand.nextInt (6)+1;
+		sum = dadu1+dadu2;
+		tries++;
+		
+		System.out.println(dadu1+ " + "+ dadu2+ " = "+sum);
+		
+		if (sum==7) {
+			System.out.println("Tebakan Anda Benar!");
+			System.out.println("Anda menang setela "+ tries+ " percobaan!");
+			running = false;
+			
+		} else {
+			System.out.println("Tebakan Anda Salah");
+			System.out.println("Apakah lanjut? (Iya/Tidak)");
+			answer= scan.nextLine();
+			
+			if (answer.equalsIgnoreCase ("tidak")) {
+				System.out.println("Anda gagal!");
+				running = false;
+			}
+		}
+	} while (running);
+	scan.close();
+	}
+}
